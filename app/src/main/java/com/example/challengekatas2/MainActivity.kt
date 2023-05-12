0760307f10ebe5446f6d1209827c5be0052fc4df
dcarrilloh <danielcarrillohcse@gmail.com>
2023-05-12 17:51:34 -0500

    Added neccesaries configurations for ui module inside app module

--- app/src/main/java/com/example/challengekatas2/MainActivity.kt
+++ app/src/main/java/com/example/challengekatas2/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.challengekatas2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.challengekatas2.ui.reminder.ReminderActivity
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -9,5 +11,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val intent = Intent(this, ReminderActivity::class.java)
+        startActivity(intent)
     }
 }
--- app/src/main/java/com/example/challengekatas2/application/ChallengeKatas2App.kt
+++ app/src/main/java/com/example/challengekatas2/application/ChallengeKatas2App.kt
@@ -4,6 +4,8 @@ import android.app.Application
 import com.example.challengekatas2.data.repository.ReminderRepository as DataReminderRepository
 import com.example.challengekatas2.domain.repository.ReminderRepository as DomainReminderRepository
 import com.example.challengekatas2.domain.usecase.*
+import com.example.challengekatas2.ui.reminder.viewmodels.CreateReminderViewModel
+import com.example.challengekatas2.ui.reminder.viewmodels.ReminderDetailViewModel
 import dagger.hilt.android.HiltAndroidApp
 import javax.inject.Inject
 
@@ -14,6 +16,14 @@ class ChallengeKatas2App : Application() {
     }
 
     // UI
+    @Inject
+    lateinit var createReminderViewModel: CreateReminderViewModel
+
+    @Inject
+    lateinit var reminderDetailViewModel: ReminderDetailViewModel
+
+    @Inject
+    lateinit var reminderListViewModel: ReminderDetailViewModel
 
     // Data
     @Inject
